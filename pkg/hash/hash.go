b97c1a5b834ea0d9313316af56ceb7ce045bbf98
Zvonko Kaiser <zkaiser@redhat.com>
2021-03-04 21:10:47 +0000

    Make kubelinter happy

--- pkg/hash/hash.go
+++ pkg/hash/hash.go
@@ -3,11 +3,16 @@ package hash
 import (
 	"fmt"
 	"hash/fnv"
+
+	"github.com/openshift-psap/special-resource-operator/pkg/exit"
+	errs "github.com/pkg/errors"
 )
 
 // FNV64a return 64bit hash
 func FNV64a(s string) string {
 	h := fnv.New64a()
-	h.Write([]byte(s))
+	if _, err := h.Write([]byte(s)); err != nil {
+		exit.OnError(errs.Wrap(err, "Could not write hash"))
+	}
 	return fmt.Sprintf("%x", h.Sum64())
 }
