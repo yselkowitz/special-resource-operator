1605697fa4c0d0267cafd24ef0e08c23da6f7c2a
Zvonko Kaiser <zkaiser@redhat.com>
2021-06-16 09:43:07 +0000

    Added helm linst

--- Makefile.helm.mk
+++ Makefile.helm.mk
@@ -1,7 +1,7 @@
 HELM_REPOS = $(shell ls -d charts/*/)
 
 
-helm-lint:
+helm-lint: helm
 	echo $(HELM_REPOS)
 	@for repo in $(HELM_REPOS); do                          \
 		cd $$repo;                              \
@@ -16,3 +16,22 @@ helm-repo-index: helm-lint
 		helm repo index . --url=file:///$$repo; \
 		cd ../..;                                      \
 	done
+
+
+
+helm:
+ifeq (, $(shell which helm))
+	@{ \
+	set -e ;\
+	HELM_GEN_TMP_DIR=$$(mktemp -d) ;\
+	cd $$HELM_GEN_TMP_DIR ;\
+	curl https://get.helm.sh/helm-v3.6.0-linux-amd64.tar.gz -o helm.tar.gz ;\
+	tar xvfpz helm.tar.gz ;\
+	mv linux-amd64/helm /usr/local/bin ;\
+	chmod +x /usr/local/bin/helm ;\
+	rm -rf $$HELM_GEN_TMP_DIR ;\
+	}
+HELM=/usr/local/bin/helm
+else
+KUSTOMIZE=$(shell which helm)
+endif
